a5782b19b9c400448cb291ccf7c5383d0f948c87
johnklawlor <john.k.lawlor@gmail.com>
2014-03-31 00:04:59 -0400

    Change carrierwave.rb to set AWS_CONFIG only in dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-AWS_CONFIG = YAML.load_file("#{::Rails.root}/config/aws.yml")[::Rails.env]
+AWS_CONFIG = YAML.load_file("#{::Rails.root}/config/aws.yml")[::Rails.env] if Rails.env==development
 
 CarrierWave.configure do |config|
 	config.fog_credentials = {
 		provider: "AWS",
-		aws_access_key_id: AWS_CONFIG["aws_key"],
-		aws_secret_access_key: AWS_CONFIG["aws_secret_key"]
+		aws_access_key_id: AWS_CONFIG["aws_key"] || ENV["aws_key"],
+		aws_secret_access_key: AWS_CONFIG["aws_secret_key"] || ENV["aws_secret_key"]
 	}
-	config.fog_directory = AWS_CONFIG["aws_bucket"]
+	config.fog_directory = AWS_CONFIG["aws_bucket"] || ENV["aws_bucket"]
 	config.fog_public = false
 end
