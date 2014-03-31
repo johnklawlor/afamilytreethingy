006e7f9d14eecaadf4b215113b22c919d1f793c2
johnklawlor <john.k.lawlor@gmail.com>
2014-03-31 00:14:03 -0400

    Exclusively use ENV variable in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,9 @@
-AWS_CONFIG = (Rails.env=="development" && YAML.load_file("#{::Rails.root}/config/aws.yml")[::Rails.env]) || nil
-
 CarrierWave.configure do |config|
 	config.fog_credentials = {
 		provider: "AWS",
-		aws_access_key_id: AWS_CONFIG["aws_key"] || ENV["aws_key"],
-		aws_secret_access_key: AWS_CONFIG["aws_secret_key"] || ENV["aws_secret_key"]
+		aws_access_key_id: ENV["aws_key"],
+		aws_secret_access_key: ENV["aws_secret_key"]
 	}
-	config.fog_directory = AWS_CONFIG["aws_bucket"] || ENV["aws_bucket"]
+	config.fog_directory = ENV["aws_bucket"]
 	config.fog_public = false
 end
