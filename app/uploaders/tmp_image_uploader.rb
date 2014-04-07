d437a4d92a74a7f6cad45e62521a83719525165c
johnklawlor <john.k.lawlor@gmail.com>
2014-04-06 22:21:41 -0400

    Change tmp_image uploader store dir to Rails.root/public/uploads

--- app/uploaders/tmp_image_uploader.rb
+++ app/uploaders/tmp_image_uploader.rb
@@ -27,7 +27,7 @@ class TmpImageUploader < CarrierWave::Uploader::Base
 	# Override the directory where uploaded files will be stored.
 	# This is a sensible default for uploaders that are meant to be mounted:
 	def store_dir
-		"#{Rails.root}/tmp/uploads"
+		"#{Rails.root}/public/uploads"
 	end
 
 	process :set_content_type
