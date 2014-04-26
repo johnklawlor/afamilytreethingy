107bb03b7900b3b0baf2ca68a2ca7d861905c41b
johnklawlor <john.k.lawlor@gmail.com>
2014-04-26 12:55:02 -0400

    Add redis heartbeat to close open EventSource/LiveStream

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,3 +2,16 @@
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 $redis = Redis.new(:url => ENV['REDISTOGO_URL'])
+
+heartbeat_thread = Thread.new do
+  while true
+    $redis.publish("comments.create","thump")
+    sleep 15.seconds
+  end
+end
+
+at_exit do
+  # not sure this is needed, but just in case
+  heartbeat_thread.kill
+  $redis.quit
+end
